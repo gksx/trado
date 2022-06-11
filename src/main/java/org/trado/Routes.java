a70e3794ff36eef553ab282bf4166a7329d2d7cc
gksx <gunnar.erlanson@gmail.com>
2022-06-11 19:52:31 +0200

    fhings

--- src/main/java/org/trado/Routes.java
+++ src/main/java/org/trado/Routes.java
@@ -4,18 +4,18 @@ import java.util.Optional;
 
 import org.trado.trie.RouteTrie;
 
-public class Routes {
+class Routes {
     private final RouteTrie routeTrie;
 
-    public Routes(){
+    Routes(){
         routeTrie = new RouteTrie();
     }
 
-    public void add(String path, String method, Action action){        
+    void add(String path, String method, Action action){        
         routeTrie.insert(pathWitNoParams(path), method, action);
     }
 
-    public Optional<Action> get(String path, String method) {
+    Optional<Action> get(String path, String method) {
         var action = routeTrie.action(pathWitNoParams(path), method);
         return action != null 
             ? Optional.of(action)
