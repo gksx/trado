a1b0549ae002902e8915fd8c3dfa4b1dc15256e1
gksx <gunnar.erlanson@gmail.com>
2023-01-12 00:09:00 +0100

    fix bug

--- src/main/java/org/trado/StaticController.java
+++ src/main/java/org/trado/StaticController.java
@@ -1,10 +1,6 @@
 package org.trado;
 
-import java.io.File;
-import java.nio.file.Files;
-
 import org.trado.controller.TradoController;
-
 
 class StaticController extends TradoController {
 
@@ -12,18 +8,13 @@ class StaticController extends TradoController {
     @Route(":fileName")
     public TradoResponse resource(TradoRequest tradoRequest){
         try {
-            var url = getClass().getClassLoader().getResource("public");
+            
             var pathArray = tradoRequest.request().uri().split("/");
             var fileName = pathArray[pathArray.length - 1];
-            var directory = new File(url.toURI());
-            File actual = null;
-            for (var file : directory.listFiles()) {
-                if (file.getName().equals(fileName)){
-                    actual = file;
-                    break;
-                }
-            }
-            return TradoResponse.raw(Files.readAllBytes(actual.toPath()))
+            
+            var bytes = getClass().getClassLoader().getResourceAsStream("public/" + fileName).readAllBytes();
+            
+            return TradoResponse.raw(bytes)
                 .contentType(convertToContentType(fileName))
                 .build();
 
@@ -39,7 +30,9 @@ class StaticController extends TradoController {
     private String convertToContentType(String fileName) {
         if(fileName == null)
             return ContentType.TEXT_PLAIN;
+
         var fileEnding = fileName.split("\\.");
+        
         switch (fileEnding[fileEnding.length-1]) {
             case "js": return ContentType.APPLICATION_JAVASCRIPT;
             case "css": return ContentType.TEXT_CSS;
