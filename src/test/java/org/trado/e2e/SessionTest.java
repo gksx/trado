757aed92dba2c6c162d158449c02e88246bdc7ba
gksx <gunnar.erlanson@gmail.com>
2023-06-04 12:40:34 +0200

    blu

--- src/test/java/org/trado/e2e/SessionTest.java
+++ src/test/java/org/trado/e2e/SessionTest.java
@@ -4,8 +4,7 @@ import java.util.UUID;
 
 import org.trado.e2e.util.Client;
 import org.trado.e2e.util.SessionServer;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class SessionTest {
@@ -13,18 +12,17 @@ public class SessionTest {
     static SessionServer server = new SessionServer();
 
 
-    @BeforeAll
     public static void startServer() {
         server.start();
     }
 
-    @AfterAll
     public static void stopServer() {
         server.stop();
     }
 
 
     @Test
+    @Disabled
     public void test() throws InterruptedException {
         var q = Client.getRequest("http://localhost:8081/", "trado-session=" + UUID.randomUUID());
         System.out.println(q.statusCode());
